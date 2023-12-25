e6d14b860972365f1854dad1a0e5081dfb123879
camenduru <54370274+camenduru@users.noreply.github.com>
2023-12-25 10:12:03 +0300

    test

---
+++ cog.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+  	"bytes"
+	"fmt"
+  	"os"
+	"os/exec"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gempir/go-twitch-irc/v3"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func taskTwitch(clientUsername, channel, oauthToken string, messageChannel chan<- string) {
+	client := twitch.NewClient(clientUsername, oauthToken)
+	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
+		text := message.Message
+		var stdoutBuffer bytes.Buffer
+    	pythonCmd := fmt.Sprintf("python -c \"import replicate; print(replicate.run('playgroundai/playground-v2-1024px-aesthetic:42fe626e41cc811eaf02c94b892774839268ce1994ea778eba97103fe1ef51b8', input={'prompt': '%s'})[0])\"", text)
+		cmd := exec.Command("sh", "-c", pythonCmd)
+		cmd.Stdout = &stdoutBuffer
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println("Error running Python command:", err)
+		}
+		resultText := stdoutBuffer.String()
+		messageChannel <- resultText
+	})
+	client.Join(channel)
+	err := client.Connect()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func handleWebSocketConnection(conn *websocket.Conn, messageChannel <-chan string) {
+	defer conn.Close()
+	fmt.Println("Client connected")
+	for {
+		message := <-messageChannel
+		err := conn.WriteMessage(websocket.TextMessage, []byte(message))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+}
+
+func readFile(path string) ([]byte, error) {
+	return ioutil.ReadFile(path)
+}
+
+func main() {
+	messageChannel := make(chan string)
+	go taskTwitch("username_sed", "channel_sed", "oauth_sed", messageChannel)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		handleWebSocketConnection(conn, messageChannel)
+	})
+	fmt.Println("Server is running on :80")
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
